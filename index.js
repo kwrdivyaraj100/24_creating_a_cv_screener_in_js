e5d150dd006281c236748100a7e893d298fb42c5
Divya R <kwrdivyaraj100@gmail.com>
2023-09-25 11:44:44 +0530

    Button listener for next button

--- index.js
+++ index.js
@@ -41,20 +41,34 @@ const data = [
         framework: 'Go framework',
         image: 'https://randomuser.me/api/portraits/men/61.jpg'
     }
-    
+
 
 
 ]
 
 // Cv Iterator
-function cvIterator(profiles){
+function cvIterator(profiles) {
     let nextIndex = 0;
-    return{
-        next: function(){
-            return nextIndex<profiles.length ?
-            {value:profiles[nextIndex++],done: false} :
-            {done:true}
+    return {
+        next: function () {
+            return nextIndex < profiles.length ?
+                { value: profiles[nextIndex++], done: false } :
+                { done: true }
 
         }
     };
+}
+
+// Button listener for next button
+const next = document.getElementById('next');
+next.addEventListener('click', nextCV);
+
+const candidates = cvIterator(data);
+
+function nextCV() {
+    const currentCandidate = candidates.next().value;
+
+    let image = document.getElementById('image');
+    let profile = document.getElementById('profile');
+    image.innerHTML = `<img src = '${currentCandidate.image}'>`
 }
