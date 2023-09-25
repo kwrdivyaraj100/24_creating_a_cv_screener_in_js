c9b56293b899a2be2fc42229ad8b24e815f64dcb
Divya R <kwrdivyaraj100@gmail.com>
2023-09-25 11:26:21 +0530

    generating random users

---
+++ index.js
@@ -0,0 +1,47 @@
+// Data is an array of which contains information about the candidates
+
+const data = [
+    {
+        name: 'Rohan Das',
+        age: 18,
+        city: 'Kolkata',
+        language: 'Python',
+        framework: 'Django',
+        image: 'https://randomuser.me/api/portraits/men/75.jpg'
+    },
+    {
+        name: 'Shubham Sharma',
+        age: 28,
+        city: 'Bangalore',
+        language: 'JavaScript',
+        framework: 'Angular',
+        image: 'https://randomuser.me/api/portraits/men/51.jpg'
+    },
+    {
+        name: 'Camella Capello',
+        age: 24,
+        city: 'US',
+        language: 'Python',
+        framework: 'Django',
+        image: 'https://randomuser.me/api/portraits/women/65.jpg'
+    },
+    {
+        name: 'Aishwarya Rai',
+        age: 45,
+        city: 'Mumbai',
+        language: 'Python',
+        framework: 'Flask',
+        image: 'https://randomuser.me/api/portraits/women/54.jpg'
+    },
+    {
+        name: 'Rohit Sharma',
+        age: 34,
+        city: 'Jharkand',
+        language: 'Go',
+        framework: 'Go framework',
+        image: 'https://randomuser.me/api/portraits/men/61.jpg'
+    }
+    
+
+
+]
